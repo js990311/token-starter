c72ebb6deb0ad2dd9c224e811d910e7c1f221e14
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-11-03 09:00:45 +0900

    init:

---
+++ src/main/java/com/rejs/token_starter/TokenStarterApplication.java
@@ -0,0 +1,13 @@
+package com.rejs.token_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rejs/token_starter/TokenStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rejs.token_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
